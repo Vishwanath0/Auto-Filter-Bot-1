ef68a6a8b9a99e159f7826936ae01050bce74603
Vishwanath <87609008+Vishwanath0@users.noreply.github.com>
2021-08-04 18:47:43 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -58,9 +58,8 @@ For More Details Click Help Button Below.
     
     ABOUT_TEXT = """<b>▷ Name</b>: Auto Filter Bot
 <b>▷ Creator </b>: <b><a href="https://t.me/vishw_anath8">Vishwanath</a></b>
-<b>▷ Supporter</b>: <b><a href="https://t.me/iZaute">Zaute Km</a></b>
 <b>▷ Language</b>: <code>Python3</code>
 <b>▷ Library</b>: <a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a>
-<b>▷ Source Code</b>: <a href="https://github.com/tgbotsProject/Auto-Filter-Bot">Click me</a>
+<b>▷ Source Code</b>: <a href="https://github.com/Vishwanath0/Auto-Filter-Bot-1">Click me</a>
 
 """
